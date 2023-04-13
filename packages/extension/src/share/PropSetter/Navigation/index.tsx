519ab61801c399736312fd9532f6ffcf4499138a
liuss <1191577401@qq.com>
2023-04-13 13:31:43 +0800

    fix: 修复废弃组件api写法

--- packages/extension/src/share/PropSetter/Navigation/index.tsx
+++ packages/extension/src/share/PropSetter/Navigation/index.tsx
@@ -13,26 +13,24 @@ export const Navigation = () => {
   const [breadcrumbList] = useStateByName('gs.propSetting.breadcrumbList', []);
 
   return <div className={styles.container}>
-    <Breadcrumb separator=">">
-      {
-        breadcrumbList.map((item, index) => {
-          return (<Breadcrumb.Item key={item.id} className={styles.breadcrumbItem}
-            onClick={() => {
-              if (index === breadcrumbList.length - 1) {
-                return;
-              } else if (index > 0) {
-                callHook(PostMessageType.OuterComponentSelect, item.id)
-              } else {
-                // 根组件不需要选择效果，直接切换，并清空标记
-                executeCommand('gc.switchIstance', item.id)
-                callHook(PostMessageType.OuterOutlineReset)
-              }
-            }}>
-            {index === 0 ? (<><HomeOutlined /></>) : item.name}
-          </Breadcrumb.Item>)
-        })
-      }
-    </Breadcrumb>
+    <Breadcrumb separator=">" items={
+      breadcrumbList.map((item, index) => {
+        return (<Breadcrumb.Item key={item.id} className={styles.breadcrumbItem}
+          onClick={() => {
+            if (index === breadcrumbList.length - 1) {
+              return;
+            } else if (index > 0) {
+              callHook(PostMessageType.OuterComponentSelect, item.id)
+            } else {
+              // 根组件不需要选择效果，直接切换，并清空标记
+              executeCommand('gc.switchIstance', item.id)
+              callHook(PostMessageType.OuterOutlineReset)
+            }
+          }}>
+          {index === 0 ? (<><HomeOutlined /></>) : item.name}
+        </Breadcrumb.Item>)
+      })
+    } />
   </div>
 }
 
